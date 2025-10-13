2e56ec79870a20422d360da0b9f4e142bb261865
Matias Arriola <matias@eyeseetea.com>
2025-10-13 09:19:55 -0300

    feat: change title size, remove "(feedback)", shrink bottom paddings

--- src/Plugin.tsx
+++ src/Plugin.tsx
@@ -23,7 +23,7 @@ const Plugin = ({ fieldsMetadata }: IDataEntryPluginProps) => {
     return (
         <Container>
             <FeedbackSection>
-                <FeedbackTitle>{`${formName} (feedback)`} </FeedbackTitle>
+                <FeedbackTitle>{formName}</FeedbackTitle>
                 {extraTexts &&
                     extraTexts.map((text, index) => (
                         <FeedbackText key={index}>{formatTextWithBoldPrefix(text)}</FeedbackText>
@@ -45,17 +45,18 @@ const Center = styled.div`
 const Container = styled.div`
     width: 100%;
     padding: 16px;
+    padding-bottom: 0;
 `;
 
 const FeedbackSection = styled.div`
     border: 1px solid #e0e5e9;
     border-radius: 4px;
     padding: 12px;
-    margin-bottom: 16px;
+    padding-bottom: 0;
 `;
 
 const FeedbackTitle = styled.div`
-    font-size: 14px;
+    font-size: 1.25rem;
     color: #212934;
     margin-bottom: 8px;
 `;
