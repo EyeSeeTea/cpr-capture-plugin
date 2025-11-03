d2d7e51389da2d723d5e89e2d706d3dd1ed9e8a7
Matias Arriola <matias@eyeseetea.com>
2025-11-03 10:39:10 -0300

    chore: change plugin name

--- d2.config.js
+++ d2.config.js
@@ -1,5 +1,5 @@
 const config = {
-    name: "extra-texts-for-options-capture-plugin",
+    name: "Capture plugin app - Extra texts for options",
     title: "Extra helper texts for options Capture Plugin",
     description: "A Capture Plugin to add extra helper texts for options.",
     type: "app",
